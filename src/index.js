f81f6ff88e733353eb0e0db730845e7a45867be7
Sahil Shah <sahilshah2104@gmail.com>
2024-03-28 00:30:01 +0530

    Feat: Added Basic Express server setup

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const { PORT } = require('./configs/server.config');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.text());
+
+app.get('/ping', (req, res) => {
+    return res.json({message : "Problem Service has started"});
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started at PORT ${PORT}`);
+});
