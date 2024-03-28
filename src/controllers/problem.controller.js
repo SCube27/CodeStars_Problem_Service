e791e47786ce9a42808a26eeb03999d544bbeeee
Sahil Shah <sahilshah2104@gmail.com>
2024-03-28 18:02:10 +0530

    Feat: Added Error Handler Middleware with Basic Error Classes

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,13 +1,18 @@
-const { StatusCodes } = require('http-status-codes')
+const { StatusCodes } = require('http-status-codes');
+const NotImplementedError = require('../errors/notImplemented.error');
+const BadRequestError = require('../errors/badrequest.error');
 
 function pingController(req, res) {
     return res.json({message : "Problem Controller is up!"});
 }
 
-function addProblem(req, res) {
-    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
-        message : 'Not Implemented'
-    });
+function addProblem(req, res, next) {
+    try {
+        throw new NotImplementedError('addProblem');
+    }
+    catch(error) {
+        next(error);
+    }
 }
 
 function getProblem(req, res) {
